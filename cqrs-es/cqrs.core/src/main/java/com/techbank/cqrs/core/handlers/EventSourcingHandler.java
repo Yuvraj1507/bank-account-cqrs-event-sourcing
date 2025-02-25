a18df4051cd291e494a0b795f00f02d4b05deb76
Yuva Raj <ydalayi8@gmail.com>
2025-02-25 22:00:49 +0530

    bank-account-cqrs-event-sourcing

---
+++ cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/handlers/EventSourcingHandler.java
@@ -0,0 +1,9 @@
+package com.techbank.cqrs.core.handlers;
+
+import com.techbank.cqrs.core.domain.AggregateRoot;
+
+public interface EventSourcingHandler<T> {
+    void save(AggregateRoot aggregate); // Liskov Substitution principle
+    T getById(String id); // return the latest state of the aggregate
+    void republishEvents();
+}
